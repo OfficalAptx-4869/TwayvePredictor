13d09797852366f4906698a9a401c34fbeeb7275
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-08-11 20:29:35 +0300

    Update Twayve.sh

--- Twayve.sh
+++ Twayve.sh
@@ -1,3 +1,23 @@
-read -p "Enter Product Code :" ProductCode
-if ( $productcode == "bDY2Hv4ZpR" , "cjMRQwG8YV" , "D9erw4nc4a" , "xugCiWbEwd" , "57PXDVKrzm" , "U4sgiBmVVW" ) then ./TPM.sh
-else ./TPEPC.sh
+#Colors 
+BLUE='\033[0;34m'
+WHITE= '\033[0;37m' 
+RED= '\033[0;31m'  
+ORANJE='\033[0;33m'
+GREEN= '\033[0;32m'  
+PURPLE='\033[0;35m'
+CYAN= '\033[0;36m'  
+
+#Login To Twayve Predictor
+echo -e "${WHITE}Login ${RED}to ${GREEN}Twayve ${CYAN}Predictor"
+echo ""
+read -p "${GREEN}Username :" username
+if [ $username == "TREX" ]; then
+clear
+read -p "${GREEN}Password :" Password
+if [ $Password == "PASSTREX" ]; then
+clear
+./TPM.sh
+else
+echo "${RED}Wrong Login informations"
+
+

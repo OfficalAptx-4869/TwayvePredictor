96c2f14221f837472efd27742e590a7bb1f4ec26
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-08-11 19:55:03 +0300

    Create Twayve.sh

---
+++ Twayve.sh
@@ -0,0 +1,3 @@
+read -p "Enter Product Code :" ProductCode
+if ( $productcode == "bDY2Hv4ZpR" , "cjMRQwG8YV" , "D9erw4nc4a" , "xugCiWbEwd" , "57PXDVKrzm" , "U4sgiBmVVW" ) then ./TPM.sh
+else ./TPEPC.sh

bac1ec3932314dbc8c9b77c231dc8c9edf31329a
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-08-11 20:33:24 +0300

    Update Twayve.sh

--- Twayve.sh
+++ Twayve.sh
@@ -1,23 +1,24 @@
 #Colors 
-BLUE='\033[0;34m'
-WHITE= '\033[0;37m' 
-RED= '\033[0;31m'  
-ORANJE='\033[0;33m'
-GREEN= '\033[0;32m'  
-PURPLE='\033[0;35m'
-CYAN= '\033[0;36m'  
+#BLUE='\033[0;34m'
+#WHITE= '\033[0;37m' 
+#RED= '\033[0;31m'  
+#ORANJE='\033[0;33m'
+#GREEN= '\033[0;32m'  
+#PURPLE='\033[0;35m'
+#CYAN= '\033[0;36m'  
 
 #Login To Twayve Predictor
-echo -e "${WHITE}Login ${RED}to ${GREEN}Twayve ${CYAN}Predictor"
+echo -e "Login to Twayve Predictor"
 echo ""
-read -p "${GREEN}Username :" username
+read -p "Username :" username
 if [ $username == "TREX" ]; then
 clear
-read -p "${GREEN}Password :" Password
+read -p "Password :" Password
 if [ $Password == "PASSTREX" ]; then
 clear
 ./TPM.sh
 else
 echo "${RED}Wrong Login informations"
+fi
 
 

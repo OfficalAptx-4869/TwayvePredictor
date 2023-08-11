c35c017aab3413904d5bf5dae2ba3afa20ee966e
OfficalAptx-4869 <133824228+OfficalAptx-4869@users.noreply.github.com>
2023-08-11 20:44:52 +0300

    Update Twayve.sh

--- Twayve.sh
+++ Twayve.sh
@@ -1,24 +1,20 @@
-#Colors 
-#BLUE='\033[0;34m'
-#WHITE= '\033[0;37m' 
-#RED= '\033[0;31m'  
-#ORANJE='\033[0;33m'
-#GREEN= '\033[0;32m'  
-#PURPLE='\033[0;35m'
-#CYAN= '\033[0;36m'  
-
-#Login To Twayve Predictor
-echo -e "Login to Twayve Predictor"
-echo ""
-read -p "Username :" username
-if [ $username == "TREX" ]; then
-clear
-read -p "Password :" Password
-if [ $Password == "PASSTREX" ]; then
-clear
-./TPM.sh
-else
-echo "${RED}Wrong Login informations"
-fi
-
-
+login_system() {
+    # Set the color codes
+    GREEN='\033[0;32m'
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    
+    # Prompt for username and password
+    read -p "Enter username: " username
+    read -sp "Enter password: " password
+    echo
+    
+    # Check if the login information is correct
+    if [ "$username" == "ADMIN" ] && [ "$password" == "TREX" ]; then
+        echo -e "${GREEN}Login successful!${NC}"
+        ./TPM.sh
+    else
+        echo -e "${RED}Login failed!${NC}"
+    fi
+}
+login_system
